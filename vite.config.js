a9ccf8e2037b97ccbbe1492fda3b68110c7450cb
Timo B <mail@timobergen.de>
2026-02-11 23:23:17 +0100

    chore: gh-pages branch

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: "/quizzie/",
   plugins: [vue()],
+  resolve: {
+    alias: [
+      
+    ]
+  },
 })
